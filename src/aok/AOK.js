e7233c152cc7ee4f4de7afad6c90da1acae0164c
Joseph Osborn <jcosborn@ucsc.edu>
2018-05-09 06:52:13 -0700

    Alter aok.js API to take a set of playspec strings rather than a set of Playspec objects, to simplify use

--- src/aok/AOK.js
+++ src/aok/AOK.js
@@ -3,6 +3,146 @@
 jt.AOK = function(emu) {
     "use strict";
 
+    var memoryAnalysisContext = {
+        tokens: [
+            {
+                type:"at",
+                match: /^at:([a-zA-Z_0-9]+)(@[0-9A-z_]+)?\((!?)([^)]+)\)/,
+                // Examples:
+                // at:mem@0x20(AF029F)
+                // at:cpu@A(u128)
+                // at:mem(s-12 DED8 m0--10--- [m1000---- m0---100- FF u35])
+                // at:mem(.* (DED8) .*) (not allowed yet)
+                // at:mem+0x80(00), at:mem+80(01), at:mem+80(02)
+                // (at:mem+0x80(00) & at:mem+FF(u10)), ..., at:mem+80(FF))
+                // TODO: allow for anchoring exactly at an offset.
+                value: function(matchResult) {
+                    var memBlock = matchResult[1];
+                    var offset = null;
+                    if(matchResult[2]) {
+                        var offsetStr = matchResult[1].substr(1);
+                        var isNumeric = false;
+                        var actualOffset = offsetStr;
+                        if(offsetStr.match(/^0x([0-9a-fA-F_]+)$/)) {
+                            isNumeric = true;
+                            actualOffset = parseInt(offsetStr.substr(2).replace("_",""), 16);
+                        } else if(offsetStr.match(/^[0-9]+$/)) {
+                            isNumeric = true;
+                            actualOffset = parseInt(offsetStr, 10);
+                        }
+                        offset = {
+                            numeric: isNumeric,
+                            actualOffset: actualOffset
+                        };
+                    }
+                    var isAnchored = matchResult[2] == "!";
+                    var pattern = [];
+                    var patternStr = matchResult[3];
+                    var inSet = false;
+                    while(patternStr.length) {
+                        //patterns can be hex digits, unums, masks, or [] unions of those
+                        var r = null;
+                        if((r = patternStr.match(/^[0-9A-Fa-f _]{2}/))) {
+                            pattern.push(hexToCode(r[0].replace("_","").replace(" ","")));
+                            patternStr = patternStr.substr(r[0].length);
+                        } else if((r = patternStr.match(/^u([0-9]+)/))) {
+                            assert(parseInt(r[1],10) < 256);
+                            pattern.push(decToCode(r[1]));
+                            patternStr = patternStr.substr(r[0].length);
+                        } else if((r = patternStr.match(/^m([01\-]{8})/))) {
+                            var sets = charCodesMatching(r[1]);
+                            if(inSet) {
+                                pattern[pattern.length-1].push(sets);
+                            } else {
+                                pattern.push(sets);
+                            }
+                            patternStr = patternStr.substr(r[0].length);
+                        } else if(patternStr[0] == "[") {
+                            inSet = true;
+                            pattern.push([]);
+                            patternStr = patternStr.substr(1);
+                        } else if(patternStr[0] == "]") {
+                            assert(inSet == true);
+                            inSet = false;
+                            patternStr = patternStr.substr(1);
+                        } else if(patternStr.match(/[\s_]/)) {
+                            patternStr = patternStr.substr(1);
+                        } else {
+                            assert(false);
+                        }
+                    }
+                    return {
+                        block:memBlock,
+                        offset:offset,
+                        pattern:pattern,
+                        anchored:isAnchored
+                    };
+                },
+                startParse: Playspecs.Parser.parseValue
+            }
+        ],
+        trace: {
+            start: function (traceData) {
+                return {index:0, consumed:true};
+            },
+            currentState: function (trace) {
+                return self.currentState;
+            },
+            advanceState: function (trace) {
+                if(!trace.consumed) {
+                    trace.index += 1;
+                    trace.consumed = true;
+                }
+            },
+            copyState: function(trace) {
+                if(!self.copyIsCurrent) {
+                    emu.aokSaveState(self.currentCopy);
+                    self.copyIsCurrent = true;
+                }
+                return self.currentCopy;
+            },
+            isStreaming: function(trace) {
+                //TODO: return false if the game is off?
+                return true;
+            },
+            isAtEnd: function (trace) {
+                return trace.consumed;
+            }
+        },
+        checks: {
+            "at": function(trace, state, idx, atNode) {
+                var memblk = state[atNode.memBlock];
+                var off = 0;
+                if(atNode.offset.isNumeric) {
+                    off = atNode.offset.actualOffset;
+                } else {
+                    memblk = memblk[atNode.offset];
+                }
+                var pat = atNode.pattern;
+                var end = memblk.length;
+                if(atNode.isAnchored) {
+                    end = off+1;
+                }
+                for(var i = off; i < end; i++) {
+                    if(bmatches(memblk[i],pat[0])) {
+                        var match = true;
+                        for(var j = i+1; j < i + pat.length; j++) {
+                            if(!bmatches(memblk[j],pat[j-i])) {
+                                match = false;
+                                break;
+                            }
+                        }
+                        if(match) {
+                            return {node:atNode, offset:i};
+                        }
+                    }
+                }
+                //TODO: don't use constant strings, use regex on bytes
+                return null;
+            }
+        }
+    };
+
     function emptyState() {
         return {'tia':null, 'ram':null, 'cpu':null};
     }
@@ -14,146 +154,6 @@ jt.AOK = function(emu) {
         self.copyIsCurrent = false;
         self.checks = [];
         self.matches = [];
-
-        var memoryAnalysisContext = {
-            tokens: [
-                {
-                    type:"at",
-                    match: /^at:([a-zA-Z_0-9]+)(@[0-9A-z_]+)?\((!?)([^)]+)\)/,
-                    // Examples:
-                    // at:mem@0x20(AF029F)
-                    // at:cpu@A(u128)
-                    // at:mem(s-12 DED8 m0--10--- [m1000---- m0---100- FF u35])
-                    // at:mem(.* (DED8) .*) (not allowed yet)
-                    // at:mem+0x80(00), at:mem+80(01), at:mem+80(02)
-                    // (at:mem+0x80(00) & at:mem+FF(u10)), ..., at:mem+80(FF))
-                    // TODO: allow for anchoring exactly at an offset.
-                    value: function(matchResult) {
-                        var memBlock = matchResult[1];
-                        var offset = null;
-                        if(matchResult[2]) {
-                            var offsetStr = matchResult[1].substr(1);
-                            var isNumeric = false;
-                            var actualOffset = offsetStr;
-                            if(offsetStr.match(/^0x([0-9a-fA-F_]+)$/)) {
-                                isNumeric = true;
-                                actualOffset = parseInt(offsetStr.substr(2).replace("_",""), 16);
-                            } else if(offsetStr.match(/^[0-9]+$/)) {
-                                isNumeric = true;
-                                actualOffset = parseInt(offsetStr, 10);
-                            }
-                            offset = {
-                                numeric: isNumeric,
-                                actualOffset: actualOffset
-                            };
-                        }
-                        var isAnchored = matchResult[2] == "!";
-                        var pattern = [];
-                        var patternStr = matchResult[3];
-                        var inSet = false;
-                        while(patternStr.length) {
-                            //patterns can be hex digits, unums, masks, or [] unions of those
-                            var r = null;
-                            if((r = patternStr.match(/^[0-9A-Fa-f _]{2}/))) {
-                                pattern.push(hexToCode(r[0].replace("_","").replace(" ","")));
-                                patternStr = patternStr.substr(r[0].length);
-                            } else if((r = patternStr.match(/^u([0-9]+)/))) {
-                                assert(parseInt(r[1],10) < 256);
-                                pattern.push(decToCode(r[1]));
-                                patternStr = patternStr.substr(r[0].length);
-                            } else if((r = patternStr.match(/^m([01\-]{8})/))) {
-                                var sets = charCodesMatching(r[1]);
-                                if(inSet) {
-                                    pattern[pattern.length-1].push(sets);
-                                } else {
-                                    pattern.push(sets);
-                                }
-                                patternStr = patternStr.substr(r[0].length);
-                            } else if(patternStr[0] == "[") {
-                                inSet = true;
-                                pattern.push([]);
-                                patternStr = patternStr.substr(1);
-                            } else if(patternStr[0] == "]") {
-                                assert(inSet == true);
-                                inSet = false;
-                                patternStr = patternStr.substr(1);
-                            } else if(patternStr.match(/[\s_]/)) {
-                                patternStr = patternStr.substr(1);
-                            } else {
-                                assert(false);
-                            }
-                        }
-                        return {
-                            block:memBlock,
-                            offset:offset,
-                            pattern:pattern,
-                            anchored:isAnchored
-                        };
-                    },
-                    startParse: Playspecs.Parser.parseValue
-                }
-            ],
-            trace: {
-                start: function (traceData) {
-                    return {index:0, consumed:true};
-                },
-                currentState: function (trace) {
-                    return self.currentState;
-                },
-                advanceState: function (trace) {
-                    if(!trace.consumed) {
-                        trace.index += 1;
-                        trace.consumed = true;
-                    }
-                },
-                copyState: function(trace) {
-                    if(!self.copyIsCurrent) {
-                        emu.aokSaveState(self.currentCopy);
-                        self.copyIsCurrent = true;
-                    }
-                    return self.currentCopy;
-                },
-                isStreaming: function(trace) {
-                    //TODO: return false if the game is off?
-                    return true;
-                },
-                isAtEnd: function (trace) {
-                    return trace.consumed;
-                }
-            },
-            checks: {
-                "at": function(trace, state, idx, atNode) {
-                    var memblk = state[atNode.memBlock];
-                    var off = 0;
-                    if(atNode.offset.isNumeric) {
-                        off = atNode.offset.actualOffset;
-                    } else {
-                        memblk = memblk[atNode.offset];
-                    }
-                    var pat = atNode.pattern;
-                    var end = memblk.length;
-                    if(atNode.isAnchored) {
-                        end = off+1;
-                    }
-                    for(var i = off; i < end; i++) {
-                        if(bmatches(memblk[i],pat[0])) {
-                            var match = true;
-                            for(var j = i+1; j < i + pat.length; j++) {
-                                if(!bmatches(memblk[j],pat[j-i])) {
-                                    match = false;
-                                    break;
-                                }
-                            }
-                            if(match) {
-                                return {node:atNode, offset:i};
-                            }
-                        }
-                    }
-                    //TODO: don't use constant strings, use regex on bytes
-                    return null;
-                }
-            }
-        };
         return self;
     }
 
@@ -224,7 +224,7 @@ jt.AOK = function(emu) {
 
     // Call this to make this instance of AOK start matching the given traces.
     // TODO AOK: You probably want to call it before the emulator starts!
-    this.startMatching = function(playspecs) {
+    this.startMatching = function(playspec_strings) {
         //atomic formula syntax for "scan through memory region for pattern":
         //at:MEM@OFF(ANCHOR PAT+)
         //Where:
@@ -244,8 +244,8 @@ jt.AOK = function(emu) {
         this.copyState = emptyState();
         this.matches = [];
         this.checks = [];
-        for(var i = 0; i < playspecs.length; i++) {
-            this.checks.push(playspecs[i].match(null, true));
+        for(var i = 0; i < playspec_strings.length; i++) {
+            this.checks.push((new Playspecs.Playspec(playspec_strings[i], memoryAnalysisContext)).match(null, "explicit"));
             this.matches.push([]);
         }
     };
